0f025db14fb410225430d94694c1e08356a86452
indrajit0909 <97145951+indrajit0909@users.noreply.github.com>
2022-03-29 12:34:19 +0530

    Update base_entry_point.py

--- stix_shifter_utils/utils/base_entry_point.py
+++ stix_shifter_utils/utils/base_entry_point.py
@@ -223,6 +223,7 @@ class BaseEntryPoint:
         self.set_delete_connector(connector)
 
     def setup_transmission_basic(self, connection, configuration):
+        print("basic idea")
         module_name = self.__connector_module
         module_path = "stix_shifter_modules." + module_name + ".stix_transmission"
         module = importlib.import_module(module_path + ".connector")
