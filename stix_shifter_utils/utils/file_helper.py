b19478fbecb88c6763583ffbaba40191b592e288
indrajit0909 <97145951+indrajit0909@users.noreply.github.com>
2022-03-30 10:14:42 +0530

    auto commit to stix_shifter_utils/utils/file_helper.py

--- stix_shifter_utils/utils/file_helper.py
+++ stix_shifter_utils/utils/file_helper.py
@@ -10,6 +10,7 @@ __default_search_path = ['stix_translation', 'json']
 
 
 def read_json(filepath, options, search_path=__default_search_path):
+    print("file helper")
     # logger.debug('call: read_json: ' + json.dumps(options, indent=4))
     # filepath may be:
     #  'to_stix_map.json' -> 'to_stix_map' mapping data if present otherwise contents of 'module'/stix_translation/json/to_stix_map.json
