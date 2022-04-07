5e51de03535ae87517cf4d73a91be40f82a24c71
indrajit0909 <97145951+indrajit0909@users.noreply.github.com>
2022-04-07 11:52:51 +0530

    Update sent_mail.py

--- sent_mail.py
+++ sent_mail.py
@@ -7,5 +7,5 @@ env_var = os.environ
   
 # Print the list of user's
 # environment variables
-print("User's Environment variables:")
+print("User's Environment variable:")
 pprint.pprint(dict(env_var), width = 1)
